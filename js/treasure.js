c4a671a6a4ffbc4c96b571ad9bfafb401e945a9b
Adrian Fernandez Carmona <adrianfdezbcn@gmail.com>
2021-08-25 13:37:03 +0200

    Source for files changed

--- js/treasure.js
+++ js/treasure.js
@@ -1,26 +1,23 @@
 class Treasure {
-    constructor(ctx, width, height, dimensionCanvas, positionX, positionY) {
-        this.ctx = ctx
-        this.treasureWidth = width
-        this.treasureHeight = height
-        this.dimensionCanvas = dimensionCanvas
-        this.treasurePositionX= positionX
-        this.treasurePositionY = positionY        
-        this.treasureImage = new Image();
-        this.treasureImage.src = `../images/treasure/treasure.png`;
-                                                     //Gets a random image
-    }
+  constructor(ctx, width, height, dimensionCanvas, positionX, positionY) {
+    this.ctx = ctx;
+    this.treasureWidth = width;
+    this.treasureHeight = height;
+    this.dimensionCanvas = dimensionCanvas;
+    this.treasurePositionX = positionX;
+    this.treasurePositionY = positionY;
+    this.treasureImage = new Image();
+    this.treasureImage.src = `../images/treasure/treasure.png`;
+    //Gets a random image
+  }
 
-
-    draw() {
-        this.ctx.drawImage(
-            this.treasureImage,
-            this.treasurePositionX,
-            this.treasurePositionY,
-            this.treasureWidth,
-            this.treasureHeight,
-            
-        );
-
-    }
+  draw() {
+    this.ctx.drawImage(
+      this.treasureImage,
+      this.treasurePositionX,
+      this.treasurePositionY,
+      this.treasureWidth,
+      this.treasureHeight
+    );
+  }
 }
